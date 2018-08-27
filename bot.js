0da0b8d834d8f54844414aad7c2d6cb9a3985b5b
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-08-27 19:11:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -420,7 +420,6 @@ client.login(process.env.BOT_TOKEN);
 
 
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
