e998675dcd71138212b2318b999188c434fa07d8
mnsor511 <42652535+mnsor511@users.noreply.github.com>
2018-08-31 02:55:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -504,16 +504,21 @@ const adminprefix = 'm-';
 
 
 
-const epic = new Discord.Client();
 
-epic.on('ready',async () => {
-  epic.channels.find(ch => ch.id === "470704098673426456" && ch.type === 'voice').join();
-});
 
-// طبعا عشان الكود يشتغل بكفاءة لازم يكون البوت مفتوح 24 ساعة
-// البوت بيخرج برا الروم اذا الخادم قفل
-// جميع الحقوق محفوظة لسيرفر كودز
 
-epic.login("التوكن")
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
